a77b34bdf80fd674e6e424988d43c4e7aa90667c
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-24 14:02:14 +0800

    classes and validations done

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :name, :address, presence: true
+end
