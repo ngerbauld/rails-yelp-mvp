100d7c1115e33e298efa0c88a0288a42e8dec3c1
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-24 16:21:56 +0800

    all functions working, see, edit, add

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
+end
